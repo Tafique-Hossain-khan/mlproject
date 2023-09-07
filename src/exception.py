787864de5df86cf1548ca53eafeb3cb2ce595448
Tafique <tafiquehossain2003@gmail.com>
2023-09-07 19:55:39 +0530

    notebook of EDA and model selection

--- src/exception.py
+++ src/exception.py
@@ -19,12 +19,6 @@ class CustomException(Exception):
         
 
 
-if __name__ == '__main__':
-
-    try:
-        a =1/0
-    except Exception as e:
-       raise CustomException(e,sys)
         
 
 
